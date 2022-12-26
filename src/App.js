cb8c8f4838e72d0a0ac2e97747e7854d787fd304
Ken Atkins <kennethdeanatkins@gmail.com>
2022-12-26 11:19:05 -0500

    Update to arrowsent logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import logo from './ArrowsEnterprisesLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        The Arrows Enterprises Website is currently in semi-permanent hiatus......
         </p>
         <a
           className="App-link"
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
